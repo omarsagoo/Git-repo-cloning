23c778eb5492c7e87f844f4607a108b94dac2bd9
goreleaserbot <goreleaser@carlosbecker.com>
2020-03-06 22:12:16 -0800

    Brew formula update for Git-repo-cloning version v1.0.9

--- Formula/Git-repo-cloning.rb
+++ Formula/Git-repo-cloning.rb
@@ -2,16 +2,16 @@
 class GitRepoCloning < Formula
   desc "MakeClones to clone repos from a google sheet"
   homepage "https://github.com/omarsagoo/Git-repo-cloning"
-  version "1.0.7"
+  version "1.0.9"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/omarsagoo/Git-repo-cloning/releases/download/v1.0.7/Git-repo-cloning_1.0.7_macOS-64bit.tar.gz"
-    sha256 "ca199dc42a3ce46ab54512158441452c94937bef88b5f5f7f72758eecbd89b83"
+    url "https://github.com/omarsagoo/Git-repo-cloning/releases/download/v1.0.9/Git-repo-cloning_1.0.9_macOS-64bit.tar.gz"
+    sha256 "72d0cf261f306d8ae8db24bcf932e9cea29ec6be14fb74ae7eab0f17bd9fedf1"
   elsif OS.linux?
   end
 
   def install
-    bin.install "makeclones"
+    bin.install "git-repo-cloning"
   end
 end
